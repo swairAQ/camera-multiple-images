f39d18cc5a2a11143a0075c5aebc5704eec7e425
swair.a <swair.a@dplit.com>
2020-12-17 13:10:06 +0500

    style changes

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -6,23 +6,16 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isVisible: false,
-            value1: null,
-            value2: null,
             dataSource: [],
             flashMode: false,
             frontCamera: false,
             showCrousal: true,
-
         }
     }
 
     render() {
         return (
             <View style={styles.subcontainer}>
-                {/* {
-          this.state.isVisible === true
-            ? */}
                 <View style={styles.container}>
                     <RNCamera
                         ref={ref => {
@@ -33,66 +26,34 @@ export default class Home extends Component {
                         flashMode={this.state.flashMode ? RNCamera.Constants.FlashMode.on : RNCamera.Constants.FlashMode.off}
                         permissionDialogTitle={'Permission to use camera'}
                         permissionDialogMessage={'We need your permission to use your camera phone'}
-
                     />
                     {
                         this.state.dataSource.length ?
                             <TouchableOpacity onPress={() =>
                                 this.props.navigation.navigate("Preview", { data: this.state.dataSource })
                             }
-                                style={{
-                                    backgroundColor: '#fff',
-                                    borderRadius: 30 / 2,
-                                    // padding: 15,
-                                    height: 30, width: 30,
-                                    // paddingHorizontal: 20,
-                                    alignSelf: 'center',
-                                    // margin: 20,
-                                    justifyContent: 'center',
-                                    position: 'absolute',
-                                    top: 15,
-                                    right: 15
-                                }}
+                                style={styles.btnPreview}
                             >
                                 <Image
                                     resizeMode='center'
-                                    style={{ height: 14, width: 14, alignSelf: 'center' }}
+                                    style={styles.ic_next}
                                     source={require("../../assets/image/next.png")} />
                             </TouchableOpacity>
                             : null
                     }
-                    <View style={{
-                        height: '20%',
-                        // width: '100%',
-                        flexDirection: 'row',
-                        // backgroundColor: 'red',
-                        justifyContent: 'center',
-                        // marginBottom: 130,
-                        borderRadius: 6
-                    }}>
+                    <View style={styles.vwContainer}>
                         <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
                             <Image
-                                style={{ height: 30, width: 30, alignSelf: 'center' }}
+                                style={styles.ic_buttons}
                                 source={require("../../assets/image/camera.png")} />
                         </TouchableOpacity>
-                        <View style={{
-                            flex: 1 / 2,
-                            marginLeft: 20,
-                            marginTop: "25%",
-                            // alignSelf: 'flex-start',
-                            alignItems: 'center',
-                            // backgroundColor: 'green',
-                            flexDirection: 'row'
-                        }}>
+                        <View style={styles.actionsContainer}>
 
                             <TouchableOpacity onPress={() => { this.setState({ flashMode: !this.state.flashMode }) }}
-                                style={{
-                                    height: 40, width: 40, backgroundColor: '#fff',
-                                    borderRadius: 40 / 2, justifyContent: 'center'
-                                }}
+                                style={styles.btnParent}
                             >
                                 <Image
-                                    style={{ height: 23, width: 23, alignSelf: 'center' }}
+                                    style={styles.ic_actions}
                                     source={
                                         this.state.flashMode ?
                                             require("../../assets/image/flash.png")
@@ -101,99 +62,68 @@ export default class Home extends Component {
                                     } />
                             </TouchableOpacity>
                             <TouchableOpacity onPress={() => { this.setState({ frontCamera: !this.state.frontCamera }) }}
-                                style={{
-                                    marginLeft: 15,
-                                    height: 40, width: 40, backgroundColor: '#fff',
-                                    borderRadius: 40 / 2, justifyContent: 'center'
-                                }}
+                                style={styles.btnParent}
                             >
                                 <Image
-                                    style={{ height: 23, width: 23, alignSelf: 'center' }}
+
+                                    style={styles.ic_actions}
                                     source={require("../../assets/image/rotate-camera.png")} />
                             </TouchableOpacity>
                         </View>
-                        <View style={{
-                            flex: 1,
-                            marginRight: 20,
-                            marginTop: '25%',
-                            justifyContent: 'flex-end',
-                            // backgroundColor: 'blue', 
-                            alignItems: 'flex-end'
-                        }}>
+                        <View style={styles.containerButton}>
                             <TouchableOpacity
                                 disabled={!this.state.dataSource.length}
                                 onPress={() => {
                                     this.setState({ showCrousal: !this.state.showCrousal })
 
                                 }}
-                                style={{
-                                    height: 40, width: 40,
+                                style={[styles.btnParent, {
                                     backgroundColor: this.state.dataSource.length ? '#fff' : 'gray',
-                                    borderRadius: 40 / 2, justifyContent: 'center'
-                                }}
+                                }]}
                             >
                                 <Image
-                                    style={{ height: 23, width: 23, alignSelf: 'center' }}
+                                    style={styles.ic_actions}
                                     source={require("../../assets/image/library.png")} />
                             </TouchableOpacity>
 
                         </View>
 
                     </View>
+
+
+                    {
+                        (this.state.showCrousal && this.state.dataSource.length) ?
+                            <View style={styles.flatContainer}>
+                                <FlatList
+                                    horizontal={true}
+                                    data={this.state.dataSource}
+                                    keyExtractor={(item, index) => item + index}
+                                    renderItem={(itemData) => {
+                                        const item = itemData.item
+                                        return (
+                                            <Image
+                                                style={styles.imgCapturedThumnail}
+                                                source={{
+                                                    uri: item
+                                                }} />
+                                        );
+                                    }} />
+                            </View>
+                            : null
+                    }
                 </View>
 
-                {
-                    (this.state.showCrousal && this.state.dataSource.length) ?
-                        <View style={{
-                            width: '100%',
-                            paddingTop: 10,
-                            backgroundColor: "black"
-                        }}>
-                            <FlatList
-                                horizontal={true}
-                                // numColumns={3}
-                                data={this.state.dataSource}
-                                keyExtractor={(item, index) => item + index}
-                                renderItem={(itemData) => {
-                                    const item = itemData.item
-                                    return (
-                                        <Image
-                                            style={styles.imgCapturedThumnail}
-                                            source={{
-                                                uri: item
-                                            }} />
-                                    );
-                                }} />
-                        </View>
-                        : null
-                }
             </View>
         );
     }
 
-    changeState = () => {
-        this.setState({ isVisible: true })
-    }
-
-    changeState2 = () => {
-        this.setState({ isVisible: true })
-    }
-
-    showResults = () => {
-        console.log('VALUE1: ' + this.state.value1);
-        console.log('VALUE2: ' + this.state.value2);
-    }
 
     takePicture = async function () {
         if (this.camera) {
             const options = { quality: 0.5, base64: true };
             const data = await this.camera.takePictureAsync(options);
-            console.log(data.uri);
             var joined = this.state.dataSource.concat(data.uri);
             this.setState({ dataSource: joined })
-
-            console.log("Array " + JSON.stringify(this.state.dataSource))
-            // this.setState({ isVisible: true });
         }
     };
 }
--- src/styles/index.js
+++ src/styles/index.js
@@ -3,12 +3,71 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    ic_actions: { height: 23, width: 23, alignSelf: 'center', },
+    btnParent: {
+        backgroundColor: 'green',
+
+        marginHorizontal: 5,
+        height: 40, width: 40, backgroundColor: '#fff',
+        borderRadius: 40 / 2, justifyContent: 'center'
+    },
+    actionsContainer: {
+        flex: 1 / 2,
+        marginLeft: 10,
+        marginTop: "25%",
+        // alignSelf: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'row',
+
+
+    },
+    containerButton: {
+        flex: 1 / 2,
+        marginRight: 20,
+        marginTop: '25%',
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+
+    },
+    ic_buttons: {
+        height: 30, width: 30, alignSelf: 'center',
+    },
+    vwContainer: {
+        height: '20%',
+        flexDirection: 'row',
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+        // marginBottom: 130,
+        borderRadius: 18,
+
+    },
+    ic_next: { height: 14, width: 14, alignSelf: 'center' },
+    btnPreview: {
+        backgroundColor: '#fff',
+        borderRadius: 30 / 2,
+        height: 30, width: 30,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 15,
+        right: 15
+    },
+    flatContainer: {
+        width: '100%',
+        marginTop: 10,
+        marginLeft: 10,
+
+    },
     txtCount: {
         fontSize: 20, color: 'gray', marginTop: '10%',
         marginBottom: '3%',
         width: '100%', textAlign: 'center'
     },
-    imgCapturedThumnail: { height: 50, width: 50, borderRadius: 7, marginHorizontal: 3, marginVertical: 5 },
+    imgCapturedThumnail: {
+        borderWidth: 0.51,
+        borderColor: '#fff',
+        height: 50, width: 50, borderRadius: 7, marginHorizontal: 3, marginVertical: 5
+    },
     imgCaptured: { height: 100, width: 100, borderRadius: 7, marginHorizontal: 3, marginVertical: 5 },
     previewContainer: {
         flex: 1,
@@ -17,7 +76,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        // borderRadius: 100,
     },
     subcontainer: {
         flex: 1,
